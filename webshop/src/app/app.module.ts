906b0aec73965173c325617d5f02cadacfe15825
AlbertAndrea <92324558+AlbertAndrea@users.noreply.github.com>
2022-02-10 19:19:45 +0100

    pages

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -7,13 +7,25 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
 import { HomeComponent } from './page/home/home.component';
 import { HeaderComponent } from './common/header/header.component';
+import { EditProductComponent } from './page/edit-product/edit-product.component';
+import { EditCustomerComponent } from './page/edit-customer/edit-customer.component';
+import { EditBillComponent } from './page/edit-bill/edit-bill.component';
+import { EditCategoryComponent } from './page/edit-category/edit-category.component';
+import { EditAddressComponent } from './page/edit-address/edit-address.component';
+import { EditOrderComponent } from './page/edit-order/edit-order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    EditProductComponent,
+    EditCustomerComponent,
+    EditBillComponent,
+    EditCategoryComponent,
+    EditAddressComponent,
+    EditOrderComponent
   ],
   imports: [
     BrowserModule,
