301042f54d1efa99f3abb212ad53986bf51af3e5
AlbertAndrea <92324558+AlbertAndrea@users.noreply.github.com>
2022-02-12 13:41:23 +0100

    bill újra

---
+++ webshop/src/app/page/bill/bill.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Bill } from 'src/app/model/bill';
+import { Customer } from 'src/app/model/customer';
+import { BillService } from 'src/app/service/bill.service';
+
+@Component({
+  selector: 'app-bill',
+  templateUrl: './bill.component.html',
+  styleUrls: ['./bill.component.scss']
+})
+export class BillComponent implements OnInit {
+
+  bills$: Observable<Bill[]> = this.billService.getAll();
+
+  constructor(
+    private billService: BillService,
+    private ar: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(bill: Bill): void {
+    this.billService.deleteItem(bill.id).subscribe(
+      ar => this.router.navigate(['/', 'bill'])
+    );
+  }
+}
